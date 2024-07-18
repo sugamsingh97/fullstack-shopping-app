575aa303bba596f5106b479b290dbeedbd3638fa
sugam <sugamsingh377@gmail.com>
2024-07-18 04:24:59 +0200

    hooking frontend with backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Login from './pages/login/Login.jsx';
+import SignUp from './pages/signup/SignUp.jsx';
+import Home from './pages/home/Home.jsx';
+import NavBar from './navbar/NavBar.jsx';
+import PageNotFound from './components/pageNotFound.jsx';
+import ProductInfo from './pages/products/ProductInfo.jsx';
+
+//import { useAuthContext } from './context/AuthContext';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  //const { authUser } = useAuthContext();
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // <div className='h-full flex justify-center'>
+    <div>
+      <Routes>
+        <Route path='/' element={<NavBar />}>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route path='login' element={<Login />}></Route>
+          <Route path='signup' element={<SignUp />}></Route>
+          <Route path='Products/product/:id' element={<ProductInfo />}></Route>
+          <Route path='*' element={<PageNotFound />}></Route>
+        </Route>
+      </Routes>
+      <Toaster />
+    </div>
+  );
 }
 
-export default App
+export default App;
